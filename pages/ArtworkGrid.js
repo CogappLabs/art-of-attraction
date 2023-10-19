64af409af48a52c357c00723538c6791e1aead3c
Hannah Baker <hannahb@cogapp.com>
2023-10-19 18:04:06 +0100

    Improve prompt and add more pink

--- pages/ArtworkGrid.js
+++ pages/ArtworkGrid.js
@@ -14,13 +14,19 @@ const ArtworkGrid = ({artworks}) => {
         console.log('Form submitted!', customOption);
     };
 
+    const answerTag = `<span>Answer: </span>`;
+
     return (
         <div className="grid grid-cols-3 gap-x-2">
             {artworks.map((artwork, index) => (
                 <div key={index}>
                     <div className="bg-pink-600 h-60 w-40"></div>
                     <h2 className="font-bold text-pink-600">Artwork Number {index+1}</h2>
-                    <div>{ artwork.answer }</div>
+                    <div>
+                        <span className="font-bold text-pink-600">{artwork.answer ? 'Answer: "' : null}</span>
+                            {artwork.answer ? artwork.answer : null}
+                        <span className="font-bold text-pink-600">{artwork.answer ? '"' : null}</span>
+                    </div>
                 </div>
             ))}
         </div>
