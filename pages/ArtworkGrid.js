30b103f53f6059d924b6b56de0dbefd2f4933059
Hannah Baker <hannahb@cogapp.com>
2023-10-20 11:56:45 +0100

    Put answers inside artwork placeholder

--- pages/ArtworkGrid.js
+++ pages/ArtworkGrid.js
@@ -8,7 +8,14 @@ const ArtworkGrid = ({artworks, isInProgress, setIsInProgress, reveal, setReveal
         <div className="grid grid-cols-3 gap-x-2">
             {artworks.map((artwork, index) => (
                 <div key={index}>
-                    <div className={ reveal[index] ? "hidden" : "bg-pink-600 h-60 w-40" }></div>
+                    <div className={ reveal[index] ? "hidden" : "bg-pink-600 h-96 w-full text-white p-4" }>
+                        <h2 className="font-bold font-xl mb-4">Artwork Number {index+1}</h2>
+                        <div className={ isInProgress ? 'hidden' : null}>
+                            <span className="font-bold">{artwork.answer ? 'Answer: "' : null}</span>
+                                {artwork.answer ? artwork.answer : null}
+                            <span className="mt-4 font-bold">{artwork.answer ? '"' : null}</span>
+                        </div>
+                    </div>
                     <div className={ reveal[index] ? "" : "hidden" }>
                         <Image 
                             src={ artwork.primaryImage }
@@ -16,12 +23,6 @@ const ArtworkGrid = ({artworks, isInProgress, setIsInProgress, reveal, setReveal
                             width={artwork.primaryImageWidth}
                             height={artwork.primaryImageHeight}
                         />
-                    </div>
-                    <h2 className="font-bold text-pink-600">Artwork Number {index+1}</h2>
-                    <div className={ isInProgress ? 'hidden' : null}>
-                        <span className="font-bold text-pink-600">{artwork.answer ? 'Answer: "' : null}</span>
-                            {artwork.answer ? artwork.answer : null}
-                        <span className="mt-4 text-pink-600 font-bold">{artwork.answer ? '"' : null}</span>
                     </div>
                 </div>
             ))}
