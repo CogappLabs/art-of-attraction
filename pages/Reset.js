1efa541ecf137bbeb56ef29d2160c07a19bccf9a
Hannah Baker <hannahb@cogapp.com>
2023-11-14 12:07:54 +0000

    Reset questions

--- pages/Reset.js
+++ pages/Reset.js
@@ -2,7 +2,7 @@ import { useState, useContext } from 'react';
 import { AppStateContext } from '../context/AppState'
 
 const Reset = () => {
-    const { setRevealImage, setRemainingQuestions, setReset, setHideReset, setSelectedOption, setButtonDisabled, setShowConfetti } = useContext(AppStateContext);
+    const { setRevealImage, setRemainingQuestions, setReset, setHideReset, setSelectedOption, setButtonDisabled, setShowConfetti, setQuestions, setBackupQuestions } = useContext(AppStateContext);
 
     const handleSubmit = (event) => {
         event.preventDefault();   
@@ -14,6 +14,18 @@ const Reset = () => {
         setSelectedOption(0);
         setButtonDisabled(false);
         setShowConfetti(false);
+
+        setQuestions([
+            'I love spending time looking at beautiful scenery, do you think I\'ll enjoy looking at you?', 
+            'My home is filled with interesting objects that each have a unique story, do you think you\'d fit in there?', 
+            'I\'m drawn to thought-provoking and emotionally charged art. Can you evoke strong feelings or reflections in me?' 
+        ]);
+
+        setBackupQuestions([        
+            "I find myself captivated by vibrant and lively atmospheres. Does your presence bring a burst of energy and color that could light up my space?",
+            "Quiet evenings with a good book are my sanctuary. Would your essence add to the tranquility and depth of such moments in my life?",
+            "I have a passion for stories and narratives that span different cultures and times. Do you carry tales or inspirations from far-off places or eras?"
+        ]);
     };
 
     return (
